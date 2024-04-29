a7479a72545b1f29f547f2e86c500d408afdcfe9
Tenseven-107 <nadrichem@tanous.nl>
2024-04-29 15:07:54 +0200

    Added a save file system

---
+++ HUG-Final_Chapter/Scripts/Utils/Backend/AbilityHandler.gd
@@ -0,0 +1,81 @@
+extends Node
+class_name AbilityHandler
+
+
+
+# Player
+export (NodePath) var player_path: NodePath
+onready var player: PlayerController = get_node(player_path)
+
+# Abilities array
+var abilities: Array = []
+export (Array, String) var variable_abilities: Array
+
+# Constants
+const save_path: String = "user://AbilitySave.save"
+const group_name: String = "AbilityHandler"
+
+
+
+# Set up
+func _ready():
+	add_to_group(group_name)
+
+	load_save()
+	set_abilities()
+
+
+
+# Write abilities
+func save():
+	var save_file: File = File.new()
+	save_file.open(save_path, File.WRITE)
+
+	save_file.store_line(JSON.print(abilities))
+	save_file.close()
+
+
+# Load the abilities save
+func load_save():
+	var save_file: File = File.new()
+	save_file.open(save_path, File.READ)
+
+	if save_file.file_exists(save_path) == true and save_file.get_len() > 0:
+		abilities = str2var(save_file.get_line())
+	else:
+		save()
+
+	save_file.close()
+
+
+# Removes the save file
+func clear_save():
+	var save_file: File = File.new()
+	save_file.open(save_path, File.WRITE)
+	save_file.close()
+
+
+
+# set up player abilities
+func set_abilities():
+	for ability in abilities:
+		for variable in variable_abilities:
+			if ability == variable:
+				player.set(variable, true)
+
+
+
+# Add ability
+func add_ability(new_ability: String):
+	if abilities.size() > 0:
+		for ability in abilities:
+			if new_ability == ability:
+				return
+
+	abilities.append(new_ability)
+	set_abilities()
+	save()
+
+
+
+
